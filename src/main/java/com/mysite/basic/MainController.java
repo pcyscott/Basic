548978ae9ae69bfe9c31df492d981fe01346f1b6
pcyscott <pcyscott@gmail.com>
2024-12-11 17:14:03 +0900

    ãb2

--- src/main/java/com/mysite/basic/MainController.java
+++ src/main/java/com/mysite/basic/MainController.java
@@ -11,4 +11,10 @@ public class MainController {
     public String index(){
       return "연습중";
     }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about(){
+        return "이러라고?";
+    }
 }
