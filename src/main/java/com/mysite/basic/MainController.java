d11f3c7179fd6f1debad1ca10f2302d4c8775266
pcyscott <pcyscott@gmail.com>
2024-12-12 10:06:36 +0900

    b4

--- src/main/java/com/mysite/basic/MainController.java
+++ src/main/java/com/mysite/basic/MainController.java
@@ -26,4 +26,27 @@ public class MainController {
     public Long AgeUp(){
         return id+age;
     }
+
+    @GetMapping("/boolean")
+    @ResponseBody
+    public boolean True(){
+        return true;
+    }
+
+    @GetMapping("/byte")
+    @ResponseBody
+    public byte Byte(){
+        return 127;
+    }
+
+    @GetMapping("/math")
+    @ResponseBody
+    public String Math(){
+
+        //return Integer.toBinaryString(45);
+        //return Integer.toOctalString(12);
+        return Integer.toHexString(167);
+    }
+
 }
+
