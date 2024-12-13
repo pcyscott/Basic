5bef0dbcb07e59414c4bbe483956e95612cd0fd8
pcyscott <pcyscott@gmail.com>
2024-12-13 15:10:22 +0900

    refactoring

--- src/main/java/com/mysite/basic/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/mysite/basic/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
@@ -2,11 +2,17 @@ package com.mysite.basic.domain.wiseSaying.wiseSaying.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @Getter
 public class WiseSaying {
+    @Setter
     private long id;
     private String content;
     private String author;
+
+    public boolean isNew() {
+        return id == 0;
+    }
 }
